2dbba0c90a5c046f1c9e4b3de6941431a445b6dc
Nathan Vander Wilt <nate@calftrail.com>
2012-02-24 15:16:46 -0800

    get CORS requests working on current CouchDB builds

--- shows/address.js
+++ shows/address.js
@@ -6,6 +6,9 @@ function (doc, req) {
         return req.peer;
     });
     provides("json", function () {
-        return {headers:{'Access-Control-Allow-Origin':"*"}, json:req.peer};
+        // NOTE: CouchDB 1.1.1 only uses {body:} or a string, see https://issues.apache.org/jira/browse/COUCHDB-1330
+        //return {headers:{'Access-Control-Allow-Origin':"*"}, json:req.peer};
+        return JSON.stringify(req.peer);
     });
+    return {headers:{'Access-Control-Allow-Origin':"*"}};   // hacky workaround for JSON issue above (does allow CORS for any type, but no matter.)
 }
